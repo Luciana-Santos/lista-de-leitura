3ee4dc0afd580890af43b884b677e64f939d0b81
Luciana Santos <luciydss@hotmail.com>
2022-10-29 19:56:47 -0300

    module class Book adicionada

---
+++ src/app/js/modules/Book.js
@@ -0,0 +1,10 @@
+export default class Book {
+  constructor(cover, title, author, pagesTotal, pagesPerDay, prevision) {
+    this.cover = cover;
+    this.title = title;
+    this.author = author;
+    this.pagesTotal = pagesTotal;
+    this.pagesPerDay = pagesPerDay;
+    this.prevision = prevision;
+  }
+}
